45af315144c5f8127d1d9eb33ffad9babba1223d
Swann HERRERA <swann@devloup.dev>
2022-12-21 19:48:25 +0100

    fix typo

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,7 +4,7 @@ import scala.util.Using
 import scala.annotation.tailrec
 
 @main def hello: Unit =
-  println(msg())
+  println(msg)
 // On Utilise un fichier de configuration
 def msg = "I was compiled by Scala 3. :)"
 
